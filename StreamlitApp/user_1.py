666af84aa4ae7be35bf31a6435ca3f29bf0af384
LonelyRandom <lonelygrowtopian@gmail.com>
2026-04-20 00:51:37 +0700

    Update user_1.py

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -946,9 +946,9 @@ def complex_film(conn, device):
         if not roles_is_empty:
             actress_role_data = []
             roles_data = film['Role']
-            roles_list = roles_data.split(' # ')
+            roles_list = roles_data.split(' ## ')
             for actress_data in roles_list:
-                actress_role = actress_data.split(', ')
+                actress_role = actress_data.split('_ ')
                 new_row = [
                     actress_role[0],
                     actress_role[1],
