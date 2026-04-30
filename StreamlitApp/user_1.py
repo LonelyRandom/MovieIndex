9dbedab5eca895ecbc30332da40456fcfc5a0ab8
LonelyRandom <lonelygrowtopian@gmail.com>
2026-04-30 23:03:33 +0700

    Update user_1.py

--- StreamlitApp/user_1.py
+++ StreamlitApp/user_1.py
@@ -1491,6 +1491,7 @@ def complex_film(conn, device):
                     if film['Cast Name'] != '--':
                         cast_data = film['Cast Name']
                         cast_list = cast_data.split(' ## ')
+                        st.write(cast_list)
                         for actress_data in cast_list:
                             actress_role = actress_data.split('_ ')
                             actress_name = cast_df.loc[cast_df['Link'] == actress_role[0], 'Target Name']
